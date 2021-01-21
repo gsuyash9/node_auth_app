e4cc7e101e2bc9a5165c42579c815ea9e006ddb6
Suyash Gupta <48146815+gsuyash9@users.noreply.github.com>
2021-01-22 01:37:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// LAST DAY
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
@@ -5,22 +6,22 @@ const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const app = express();
 
-// middleware
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection
+
 const dbURI = 'mongodb+srv://myfirstdb:993565@cluster0.i5wkz.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
-app.get('*', checkUser);
-app.get('/', (req, res) => res.render('home'));
+app.get('*',checkUser);
+app.get('/',(req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
